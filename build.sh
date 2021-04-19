288d034beff17bb8814599a8d9b3b7ff1251dddb
Lee, JunHyuk <leejh10003@gmail.com>
2021-04-19 23:53:03 +0900

    Source

--- build.sh
+++ build.sh
@@ -2,4 +2,5 @@
 cd /home/ubuntu &&
 unzip embedded-team-backend.zip &&
 rm embedded-team-backend.zip &&
-/home/ubuntu/.nvm/versions/node/v14.16.1/bin/yarn install
+. /home/ubuntu/.bashrc &&
+yarn install
