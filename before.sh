ce6ea966652ee6e22c0c29143e16bb59f45f6012
Lee, JunHyuk <leejh10003@gmail.com>
2021-04-19 23:48:54 +0900

    Check existence

--- before.sh
+++ before.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-rm -r /home/ubuntu/node_modules &&
-rm /home/ubuntu/README.md &&
-rm /home/ubuntu/buildspec.yml &&
-rm /home/ubuntu/index.js &&
-rm /home/ubuntu/package.json &&
-rm /home/ubuntu/yarn.lock
+[ -e /home/ubuntu/node_modules ] && rm -r /home/ubuntu/node_modules
+[ -e /home/ubuntu/README.md ] && rm /home/ubuntu/README.md
+[ -e /home/ubuntu/buildspec.yml ] && rm /home/ubuntu/buildspec.yml
+[ -e /home/ubuntu/index.js ] && rm  /home/ubuntu/index.js
+[ -e /home/ubuntu/package.json ] && rm  /home/ubuntu/package.json
+[ -e /home/ubuntu/yarn.lock ] && rm  /home/ubuntu/yarn.lock
