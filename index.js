0c7a777c67a636c6fd7e97aa80612612fe476305
Lee, JunHyuk <leejh10003@gmail.com>
2021-04-19 20:20:15 +0900

    Fix http request response

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const router = new KoaRouter();
 app.use(BodyParser());
 router.options('healthPreflight', '/health', async (ctx) => {
 	ctx.set('Access-Control-Allow-Origin', ctx.request.header.referer || ctx.request.header.origin);
-	ctx.set('Access-Control-Allow-Methods', 'OPTIONS, POST');
+	ctx.set('Access-Control-Allow-Methods', 'OPTIONS, POST, GET');
 	ctx.set('Access-Control-Allow-Headers', 'Access-Control-Allow-Origin, Content-Type, Authorization');
   ctx.set('Access-Control-Allow-Credentials', true);
 	ctx.response.status = 200;
@@ -16,7 +16,7 @@ router.get('health', '/health', async (ctx) => {
   ctx.response.body = 'OK';
   ctx.response.status = 200;
   ctx.set('Access-Control-Allow-Origin', ctx.request.header.referer || ctx.request.header.origin);
-  ctx.set('Access-Control-Allow-Credentials', 'true');
+  ctx.set('Access-Control-Allow-Credentials', true);
 })
 app.use(router.routes())
   .use(router.allowedMethods())
