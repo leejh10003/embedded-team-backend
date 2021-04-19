9ac39fc8f73d97008def718881e6e67f14f29e6c
Lee, JunHyuk <leejh10003@gmail.com>
2021-04-20 00:14:42 +0900

    Add sourcing

--- build.sh
+++ build.sh
@@ -2,6 +2,8 @@
 cd /home/ubuntu
 unzip embedded-team-backend.zip
 rm embedded-team-backend.zip
-source /home/ubuntu/.bashrc
+\. ~/.nvm/nvm.sh
+\. ~/.profile
+\. ~/.bashrc
 nvm use 14.16.1
 yarn install
