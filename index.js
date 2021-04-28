5d401bb3c43f39d50859d116ef811faef4fd58b6
Lee, JunHyuk <leejh10003@gmail.com>
2021-04-29 00:35:11 +0900

    Add implementation

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
+require('dotenv').config()
 const Koa = require('koa');
 const KoaRouter = require('koa-router');
 const BodyParser = require('koa-bodyparser');
 const app = new Koa();
+const vision = require('@google-cloud/vision');
+const multer = require("@koa/multer");
+const jsQR = require("jsqr");
+var sizeOf = require('image-size');
+const client = new vision.ImageAnnotatorClient();
+const Jimp = require('jimp');
+const upload = multer({
+    storage: multer.memoryStorage()
+});
 app.proxy = true;
 const router = new KoaRouter();
 app.use(BodyParser());
@@ -17,6 +27,53 @@ router.get('health', '/health', async (ctx) => {
   ctx.response.status = 200;
   ctx.set('Access-Control-Allow-Origin', ctx.request.header.referer || ctx.request.header.origin);
   ctx.set('Access-Control-Allow-Credentials', true);
+})
+router.post('qrcode', '/qrcode', upload.fields([{
+  name: 'file'
+}]), async ctx => {
+  if (!!(ctx.request.files) && (ctx.request.files.file.length > 0)){
+    const content = ctx.request.files.file[0].buffer
+    const image = await Jimp.read(content)
+    const pixels = new Uint8ClampedArray(image.bitmap.data)
+    
+    const code = jsQR(pixels, image.bitmap.width, image.bitmap.height);
+    ctx.response.body = {
+      data: code.data
+    }
+  }
+})
+router.post('food', '/food', upload.fields([{
+  name: 'file'
+}]), async (ctx) => {
+  if (!!(ctx.request.files) && (ctx.request.files.file.length > 0)){
+    const content = ctx.request.files.file[0].buffer.toString('base64')
+    const request = {
+      image: {content: content },
+    }
+    const [result] = await client.objectLocalization(request);
+    const objects = result.localizedObjectAnnotations;
+    const foods = {}
+    let most = 0;
+    let name = null;
+    objects.filter(object => object.score >= 0.5).forEach(object => {
+      if (!!(foods[object.name])){
+        foods[object.name] += 1
+      } else {
+        foods[object.name] = 1
+      }
+    });
+    console.log(foods)
+    for (var key in foods){
+      if (foods[key] > most){
+        name = key
+        most = foods[key]
+      }
+    }
+    ctx.response.body = {
+      name,
+      count: most
+    }
+  }
 })
 app.use(router.routes())
   .use(router.allowedMethods())
