0a8fa336b57c015168fc4d0a88cbeb5e5c37a894
SSZ4 <37368388+SSZ4@users.noreply.github.com>
2021-04-29 20:14:37 +0900

    ok -> hello

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 require('dotenv').config()
-const Koa = require('koa');
-const KoaRouter = require('koa-router');
-const BodyParser = require('koa-bodyparser');
-const app = new Koa();
-const vision = require('@google-cloud/vision');
-const multer = require("@koa/multer");
-const jsQR = require("jsqr");
-var sizeOf = require('image-size');
-const client = new vision.ImageAnnotatorClient();
-const Jimp = require('jimp');
+const Koa = require('koa'); //Koa http server Framework
+const KoaRouter = require('koa-router'); //Koa path route middleware
+const BodyParser = require('koa-bodyparser'); //Koa Body parse middleware
+const app = new Koa(); //Create Koa app instance
+const vision = require('@google-cloud/vision'); //Import google cloud vision module
+const multer = require("@koa/multer"); //Koa multer multipart upload extension
+const jsQR = require("jsqr"); //Pure javascript QR code decode module
+const client = new vision.ImageAnnotatorClient(); //구글에 요청보낼 때 사용
+const Jimp = require('jimp');//For jsqr, have to handle image
 const upload = multer({
     storage: multer.memoryStorage()
 });
@@ -23,13 +22,13 @@ router.options('healthPreflight', '/health', async (ctx) => {
 	ctx.response.status = 200;
 })
 router.get('health', '/health', async (ctx) => {
-  ctx.response.body = 'OK';
+  ctx.response.body = 'hello';
   ctx.response.status = 200;
   ctx.set('Access-Control-Allow-Origin', ctx.request.header.referer || ctx.request.header.origin);
   ctx.set('Access-Control-Allow-Credentials', true);
 })
 router.post('qrcode', '/qrcode', upload.fields([{
-  name: 'file'
+  name: 'file' 
 }]), async ctx => {
   try {
     if (!!(ctx.request.files) && (ctx.request.files.file.length > 0)){
@@ -37,7 +36,7 @@ router.post('qrcode', '/qrcode', upload.fields([{
       const image = await Jimp.read(content)
       const pixels = new Uint8ClampedArray(image.bitmap.data)
       
-      const code = jsQR(pixels, image.bitmap.width, image.bitmap.height);
+      const code = jsQR(pixels, image.bitmap.width, image.bitmap.height);//jsqr은 Uint8ClampedArray 형식만 받음.
       ctx.response.body = {
         success: true,
         data: code.data
@@ -62,12 +61,12 @@ router.post('food', '/food', upload.fields([{
       const request = {
         image: {content: content },
       }
-      const [result] = await client.objectLocalization(request);
-      const objects = result.localizedObjectAnnotations;
-      const foods = {}
+      const [result] = await client.objectLocalization(request); //[result]는 array의 첫 번째 값이 들어옴.
+      const objects = result.localizedObjectAnnotations; //이미지를 해석한 결과 -> score, name, ...
+      const foods = {} //객체
       let most = 0;
       let name = null;
-      objects.filter(object => object.score >= 0.5).forEach(object => {
+      objects.filter(object => object.score >= 0.5).forEach(object => { 
         if (!!(foods[object.name])){
           foods[object.name] += 1
         } else {
@@ -82,9 +81,9 @@ router.post('food', '/food', upload.fields([{
       }
       ctx.response.body = {
         success: true,
-        name,
+        name, // name: name,
         count: most,
-        max_availability: 10
+        max_availability: 10 // 식품 유효기간? 유통기한
       }
     } else {
       throw new Error('file not provided')
